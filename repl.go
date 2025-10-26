5651874c22ccdc6c1cc3a8a9aa16d9ff2a209f75
Kelly Cantrell <kellycantrelly@gmail.com>
2025-10-26 10:19:01 -0500

    Refactored some bits, moved some functions to seperate file.

--- repl.go
+++ repl.go
@@ -2,48 +2,38 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 )
 
-func startRepl() {
-	reader := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		reader.Scan()
-
-		words := cleanInput(reader.Text())
-		if len(words) == 0 {
-			continue
-		}
-
-		commandName := words[0]
-
-		command, exists := getCommands()[commandName]
-		if exists {
-			err := command.callback()
-			if err != nil {
-				fmt.Println(err)
-			}
-			continue
-		} else {
-			fmt.Println("Unknown command")
-			continue
-		}
-	}
-}
-
-func cleanInput(text string) []string {
-	output := strings.ToLower(text)
-	words := strings.Fields(output)
-	return words
-}
-
+// Structs
 type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
+}
+
+// Functions
+func cleanInput(text string) []string {
+	splitString := strings.Fields(strings.ToLower(text))
+	return splitString
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return errors.New("Closing")
+}
+
+func commandHelp() error {
+	commands := getCommands()
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return errors.New("Help")
 }
 
 func getCommands() map[string]cliCommand {
@@ -55,8 +45,31 @@ func getCommands() map[string]cliCommand {
 		},
 		"exit": {
 			name:        "exit",
-			description: "Exit the Pokedex",
+			description: "Exit the pokedex",
 			callback:    commandExit,
 		},
+	}
+}
+
+// End of Functions
+
+func startRepl() {
+	commands := getCommands()
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Println("Read error: ", err)
+			}
+		}
+
+		userInput := scanner.Text()
+		processedInput := cleanInput(userInput)
+
+		command := processedInput[0]
+		if value, ok := commands[command]; ok {
+			value.callback()
+		}
 	}
 }
