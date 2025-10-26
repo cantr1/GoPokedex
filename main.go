7c9b1dc05610c0fc15c8ba9aa3dc3c81041bce3d
Kelly Cantrell <kellycantrelly@gmail.com>
2025-10-26 10:12:16 -0500

    Added repl feature

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func cleanInput(text string) []string {
-	splitString := strings.Fields(strings.ToLower(text))
-	return splitString
-}
-
 func main() {
-	for {
-		fmt.Print("Pokedex > ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		userInput := scanner.Text()
-		processedInput := cleanInput(userInput)
-		fmt.Printf("Your command was: %v\n", processedInput[0])
-
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("Error reading input: %v\n", err)
-		}
-	}
+	startRepl()
 }
